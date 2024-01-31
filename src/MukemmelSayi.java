020759c63a89d2f66a4e66e4f13e6bae661ad88e
Medine <mhedekoglu20@gmail.com>
2024-01-31 22:11:36 +0300

    MukemmelSayi

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+        
+        int toplam = 0;
+        for(int i = 1; i <= sayi; i++){
+            
+            if(sayi % i == 0){
+                toplam =+ i;
+            }
+            
+        }
+        
+        if(toplam == sayi && sayi != 1){
+            System.out.println(sayi + " Mükemmel sayıdır...");
+        }
+        else{
+            System.out.println(sayi + " Mükemmel sayı değildir...");
+        }
+        
+        
+    }
+}
